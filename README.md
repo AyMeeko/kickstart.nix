6042710f562a88cfc8ed94eb214d6bc4e9844559
AyMeeko <87551537+AyMeeko@users.noreply.github.com>
2024-06-29 18:04:22 -0500

    getting nvim to work kinda

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ https://github.com/Evertras/simple-homemanager/tree/main
 This is for fixing wezterm installation and it seems like the PR is close to being merged?
 https://github.com/nix-community/nixGL
 https://github.com/nix-community/home-manager/issues/3968
+
+## Setting shell
+home-manager isn't able to switch the default shell to zsh.
+In PopOS Terminal, you can set it to just run `zsh` when opening
+
+## telescope-fzf-native
+
+For some reason, you have to cd into
+```
+.local/share/nvim/lazy/telescope-fzf-native.nvim
+```
+and run `make`
--- system/common.nix
+++ system/common.nix
@@ -1,15 +1,85 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   home = {
     packages = with pkgs; [
       bat
+      fd
       fzf
+      fzf-zsh
+      gcc
+      gnumake42
       home-manager
-      neovim
       ripgrep
       tmux
     ];
 
     stateVersion = "23.11";
+
+    file."./.config/nvim/" = {
+      source = ../config/nvim;
+      recursive = true;
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = {
+      "vim" = "nvim";
+    };
+    syntaxHighlighting = {
+      enable = true;
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    #extraConfig = ":luafile ~/.config/nvim/init.lua";
+    extraPackages = [
+      # Included to build telescope-fzf-native.nvim
+      pkgs.cmake
+      # Included for LuaSnip
+      pkgs.luajitPackages.jsregexp
+    ];
+    withNodeJs = true;
+    withPython3 = true;
+    withRuby = true;
+    vimdiffAlias = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    colors = {
+      bg = "#24273a";
+      "bg+" = "#363a4f";
+      spinner = "#f4dbd6";
+      hl = "#ed8796";
+      fg = "#cad3f5";
+      header = "#ed8796";
+      info = "#c6a0f6";
+      pointer = "#f4dbd6";
+      marker = "#f4dbd6";
+      "fg+" = "#cad3f5";
+      prompt = "#c6a0f6";
+      "hl+" = "#ed8796";
+    };
+    defaultOptions = [
+      "--bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down"
+      "--preview 'cat {}'"
+    ];
+  };
+
+  programs.git = {
+    enable = true;
+    aliases = {
+      gc = "git commit";
+      gs = "git status";
+      gdc = "git diff --cached";
+      gd = "git diff";
+    };
   };
 }
