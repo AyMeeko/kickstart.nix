6ebd30408c96e07c1933c3844fc9ef922a075667
AyMeeko <87551537+AyMeeko@users.noreply.github.com>
2024-06-29 21:06:11 -0500

    starship

--- system/common.nix
+++ system/common.nix
@@ -110,4 +110,46 @@ in
     viAlias = true;
     vimAlias = true;
   };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = pkgs.lib.importTOML ../config/starship.toml;
+    #settings = {
+      #buf = {
+        #disabled = true;
+      #};
+      #character = {
+        #success_symbol = "[󰘧](bold green)";
+        #error_symbol = "[󰘧](bold red)";
+      #};
+      #directory = {
+        #truncate_to_repo = false;
+      #};
+      #dotnet = {
+        #detect_files = [
+          #"global.json"
+          #"Directory.Build.props"
+          #"Directory.Build.targets"
+          #"Packages.props"
+        #];
+      #};
+      #git_branch = {
+        #symbol = " ";
+        #truncation_length = 18;
+      #};
+      #golang = {
+        #symbol = " ";
+      #};
+      #lua = {
+        #symbol = " ";
+      #};
+      #nix_shell = {
+        #symbol = " ";
+      #};
+      #package = {
+        #disabled = true;
+      #};
+    #};
+  };
 }
