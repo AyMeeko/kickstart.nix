73428766308727ea06f7212d69fc1bd665cfb9a5
AyMeeko <87551537+AyMeeko@users.noreply.github.com>
2024-09-10 11:31:19 -0500

    add opendyslexic fonts

--- lib/fonts.nix
+++ lib/fonts.nix
@@ -1,15 +1,17 @@
 {stdenvNoCC}:
 stdenvNoCC.mkDerivation rec {
   pname = "premium-fonts";
-  version = "1.0.0";
+  version = "1.1.0";
   name = "${pname}-${version}";
   src = builtins.fetchGit {
     url = "git@github.com:aymeeko/fonts.git";
-    rev = "738a81ba294490d3f1a7428f3aa743024565498e";
+    rev = "00d11917888de866b6dcd2c53e551953354f09ec";
   };
 
   installPhase = ''
     install -Dm444 CascadiaCode/CascadiaCode-2404.23/ttf/*.ttf -t $out/share/fonts
     install -Dm444 MonoLisa/MonoLisa-custom1-2.015/ttf/*.ttf -t $out/share/fonts
+    install -Dm444 OpenDyslexic/OpenDyslexicMono/*.otf -t $out/share/fonts
+    install -Dm444 OpenDyslexic/OpenDyslexic3/*.ttf -t $out/share/fonts
   '';
 }
