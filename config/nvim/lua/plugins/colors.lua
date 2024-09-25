8ba2527582292e4bb473f28ce83c59d5349900b9
AyMeeko <87551537+AyMeeko@users.noreply.github.com>
2024-09-25 18:52:39 -0500

    remove unnecessary nvim colorschemes

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -1,10 +1,5 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    event = "VeryLazy",
-    opts = {},
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
@@ -56,40 +51,4 @@ return {
       vim.cmd.colorscheme "catppuccin"
     end,
   },
-  {
-    "navarasu/onedark.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      {
-        "norcalli/nvim-colorizer.lua",
-        event = "VeryLazy"
-      }
-    },
-    config = function()
-      require("onedark").setup({
-        transparent = true,
-        diagnostics = {
-          background = false,
-        },
-        --style = "dark",
-      })
-      --vim.cmd.colorscheme("onedark")
-      --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-      --vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-
-      -- headlines.nvim
-      --vim.api.nvim_set_hl(0, 'Headline1', {fg = '#cb7676', bg = '#402626', italic = false})
-      --vim.api.nvim_set_hl(0, 'Headline2', {fg = '#c99076', bg = '#66493c', italic = false})
-      --vim.api.nvim_set_hl(0, 'Headline3', {fg = '#80a665', bg = '#3d4f2f', italic = false})
-      --vim.api.nvim_set_hl(0, 'Headline4', {fg = '#4c9a91', bg = '#224541', italic = false})
-      --vim.api.nvim_set_hl(0, 'Headline5', {fg = '#6893bf', bg = '#2b3d4f', italic = false})
-      --vim.api.nvim_set_hl(0, 'Headline6', {fg = '#d3869b', bg = '#6b454f', italic = false})
-      --vim.api.nvim_set_hl(0, 'CodeBlock', {bg = '#31353f'})
-
-      require('colorizer').setup({
-        filetypes = { "*", "!TelescopeResults" },
-      })
-    end
-  }
 }
